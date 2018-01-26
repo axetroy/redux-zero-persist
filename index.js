63c86f23339e09ae787699ca77863bbab4d373a9
axetroy <troy450409405@gmail.com>
2018-01-26 17:45:53 +0800

    chore: Build with libpack

--- index.js
+++ index.js
@@ -1,72 +1,59 @@
 const key_prefix = "persist:";
 
-function _() {}
-
 function mapStateToStorage(store, config) {
   const state = store.getState();
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     config.storage.setItem(
       key_prefix + config.key,
       JSON.stringify(state),
-      function(err) {
-        err ? reject(err) : resolve(state);
-      }
+      err => (err ? reject(err) : resolve(state))
     );
   });
 }
 
 function mapStorageToState(state, config, cb) {
-  config.storage.getItem(key_prefix + config.key, function(err, value) {
-    cb(err, Object.assign({}, state, JSON.parse(value)));
-  });
+  config.storage.getItem(key_prefix + config.key, (err, value) =>
+    cb(err, Object.assign({}, state, JSON.parse(value)))
+  );
 }
 
 function Storage() {}
 
 Storage.prototype.getItem = function(key, cb) {
   const val = localStorage.getItem(key);
-  setTimeout(function() {
-    cb(null, val);
-  });
+  setTimeout(() => cb(null, val));
 };
 
 Storage.prototype.setItem = function(key, item, cb) {
   localStorage.setItem(key, item);
-  setTimeout(function() {
-    cb(null, val);
-  });
+  setTimeout(() => cb(null, val));
 };
 
 Storage.prototype.removeItem = function(key) {
   localStorage.removeItem(key);
-  setTimeout(function() {
-    cb(null);
-  });
+  setTimeout(() => cb(null));
 };
 
-function persist(config, cb) {
-  cb = cb || _;
-  config = config || { key: "[rz]", storage: new Storage() };
+function persist(
+  config = { key: "[rc]", storage: new Storage() },
+  cb = () => {}
+) {
   mapStorageToState({}, config, cb);
 
   // return middleware
-  return function(store) {
-    return function(next) {
-      return function(action) {
-        const r = next(action);
-        if (r && typeof r.then === "function") {
-          return next(action).then(function(d) {
-            return mapStateToStorage(store, config).then(function() {
-              return Promise.resolve(d);
-            });
-          });
-        } else {
-          return mapStateToStorage(store, config).then(function() {
-            return Promise.resolve(r);
-          });
-        }
-      };
-    };
+  return state => next => action => {
+    const r = next(action);
+    if (r && typeof r.then === "function") {
+      return next(action).then(d => {
+        return mapStateToStorage(store, config).then(function() {
+          return Promise.resolve(d);
+        });
+      });
+    } else {
+      return mapStateToStorage(store, config).then(function() {
+        return Promise.resolve(r);
+      });
+    }
   };
 }
 
